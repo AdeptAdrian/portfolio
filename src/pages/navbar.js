8d886760e4fb6662fec1426b9ef6ebb190dac772
Adrian <adrian.01@list.ru>
2024-11-20 20:46:10 -0600

    Update navbar.js

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,19 +1,19 @@
 function navbar() {
 	return (
 		<>
-			<div className="fixed left-1/2 -translate-x-1/2 w-full md:w-auto">
+			<div className="fixed top-10 left-1/2 -translate-x-1/2 w-full md:w-auto">
 				<nav className="relative ">
-					<ul className="relative grid grid-cols-4 items-center">
-						<li className="relative px-2 py-2 text-center cursor-pointer border border-white/10 backdrop-blur-md rounded-full text-neutral-300">
+					<ul className="relative grid grid-cols-4 items-center border border-white/10 rounded-full backdrop-blur-md py-2 shadow-lg mx-2 md:mx-2">
+						<li className="relative md:px-4 px-2 py-2 text-center cursor-pointer  text-neutral-300">
 							<a href="#">Profile</a>
 						</li>
-						<li className="relative px-2 py-2 text-center cursor-pointer border border-white/10 backdrop-blur-md rounded-full text-neutral-300">
+						<li className="relative md:px-4 px-2 py-2 text-center cursor-pointer text-neutral-300">
 							<a href="#stack">My Stack</a>
 						</li>
-						<li className="relative px-2 py-2 text-center cursor-pointer border border-white/10 backdrop-blur-md rounded-full text-neutral-300">
+						<li className="relative md:px-4 px-2 py-2 text-center cursor-pointer text-neutral-300">
 							<a href="#projects">My Projects</a>
 						</li>
-						<li className="relative px-2 py-2 text-center cursor-pointer border border-white/10 backdrop-blur-md rounded-full text-neutral-300">
+						<li className="relative md:px-4 px-2 py-2 text-center cursor-pointer text-neutral-300">
 							<a href="#aboutme">About Me</a>
 						</li>
 					</ul>
